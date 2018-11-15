c445a61a6e105c902dc82571c06c9ca6ad0a1b2a
agent782 <79206167111@yandex.ru>
2018-11-15 11:05:56 +0300

    2

--- views/client/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Admin
- * Date: 02.02.2018
- * Time: 17:20
- */
-var_dump($modelCompany);
--- web/js/test.js
+++
@@ -1,4 +0,0 @@
-
-$(document).ready(function(){
-    alert('Ваша версия jQuery ' + jQuery.fn.jquery);
-});
