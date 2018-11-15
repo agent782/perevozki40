eb0fdbc10b9d9c5fbdb78912e1e0ec18448b059e
agent782 <79206167111@yandex.ru>
2018-11-15 11:00:13 +0300

    22

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=grigorov.org;dbname=FORSAG_BASE',
-    'username' => 'root2',
-    'password' => '250640',
-    'charset' => 'utf8',
-];
--- config/params.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => '79206167111@yandex.ru',
-    'user.passwordResetTokenExpire' => 3600,
-    'supportEmail' => '79206167111@yandex.ru',
-];
