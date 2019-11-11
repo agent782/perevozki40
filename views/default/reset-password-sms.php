74048fe9be6391956e6dc6480da338e93de92cb1
work <79206167111@ya.ru>
2019-11-11 16:39:24 +0300

    bbb

--- views/default/reset-password-sms.php
+++ views/default/reset-password-sms.php
@@ -20,28 +20,28 @@ use yii\helpers\Url;
     <?php
         $form = ActiveForm::begin();
     ?>
-    <?= $form->field($model, 'phone')
-        ->textInput(['autofocus' => true])
-        ->widget(MaskedInput::className(),[
-            'mask' => '+7(999)999-99-99',
-            'clientOptions'=>[
-                'removeMaskOnSubmit' => true,
-            ],
-            'options' => [
-                'type' => 'tel',
-            ]
-        ])
-    ?>
+<!--    --><?//= $form->field($model, 'phone')
+//        ->textInput(['autofocus' => true])
+//        ->widget(MaskedInput::className(),[
+//            'mask' => '+7(999)999-99-99',
+//            'clientOptions'=>[
+//                'removeMaskOnSubmit' => true,
+//            ],
+//            'options' => [
+//                'type' => 'tel',
+//            ]
+//        ])
+//    ?>
     <?= $form->field($model, 'captcha')->widget(Captcha::class, [
 //        'captchaAction' => 'default/captcha'
     ])?>
-    <?= Html::submitButton('Отправить код по СМС', ['class' => 'btn btn-info', 'name' => 'button', 'value' => 'send_sms'])?>
-    <br><br>
-    <?= Html::submitButton('У меня есть код из СМС', [
-        'class'=>'btn btn-primary',
-        'name' => 'button',
-        'value' => 'change-password'
-    ])?>
+<!--    --><?//= Html::submitButton('Отправить код по СМС', ['class' => 'btn btn-info', 'name' => 'button', 'value' => 'send_sms'])?>
+<!--    <br><br>-->
+<!--    --><?//= Html::submitButton('У меня есть код из СМС', [
+//        'class'=>'btn btn-primary',
+//        'name' => 'button',
+//        'value' => 'change-password'
+//    ])?>
     <?php
         $form::end();
     ?>
