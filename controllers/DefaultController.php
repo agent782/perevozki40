0e316d7a065a2101d52fd0471eaece5707aa5bfa
work <79206167111@ya.ru>
2019-11-15 08:52:48 +0300

    bbb

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -112,7 +112,7 @@ class DefaultController extends Controller
 
     //Регистрация
     public function actionSignup(){
-        return $this->redirect('/');
+//        return $this->redirect('/');
 
         $modelUser = new User();
         $modelProfile = new Profile();
