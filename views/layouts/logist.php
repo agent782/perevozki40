0456cc988c9b40bae82913e555b9025304e56ee6
work <79206167111@ya.ru>
2019-12-05 09:41:21 +0300

    bbb

--- views/layouts/logist.php
+++ views/layouts/logist.php
@@ -41,50 +41,7 @@ AppAsset::register($this);
             'id' => 'menu',
         ],
     ]);
-    echo AutoComplete::widget([
-        'clientOptions' => [
-            'source' => \app\models\Profile::getArrayForAutoComplete(false),
-            'autoFill' => true,
-            'minLength' => '0',
-            'select' => new JsExpression('function(event, ui) {
-                location.href = $("#urlTo").val() + "/?id=" + ui.item.id;
-               $("#label").html("Клиент");
-               $("#id").val(ui.item.id);
-//               alert($(this).val());
-               $("#username").val(ui.item.phone);
-               $("#phone2").val(ui.item.phone2);
-               $("#email").val(ui.item.email);
-               $("#email2").val(ui.item.email2);
-               $("#name").val(ui.item.name);
-               $("#surname").val(ui.item.surname);
-               $("#patrinimic").val(ui.item.patrinimic);
-               var id = ui.item.id;
-            }'),
-            'response' => new JsExpression('function(event, ui) {
-               $("#username").val($(this).val());
-            }'),
-            'change' => new JsExpression('function(event, ui) {
-                if(!ui.item) {
-                     $("#label").html("Новый клиент");
-                       $("#id").val("");
-//                       $("#username").val("");
-                       $("#phone2").val("");
-                       $("#email").val("");
-                       $("#email2").val("");
-                       $("#name").val("");
-                       $("#surname").val("");
-                       $("#patrinimic").val("");
-                       $("#surname").focus();
-                        
-                }
-            }'),
-        ],
-        'options' => [
-            'id' => 'search-all',
-            'class' => 'form-control',
-            'placeholder' => Yii::t('app', 'Поиск...'),
-        ]
-    ]);
+
     echo Nav::widget([
         'encodeLabels' => false,
         'options' => [
@@ -129,7 +86,52 @@ AppAsset::register($this);
 
     NavBar::end();
     ?>
+    <?=
+    echo AutoComplete::widget([
+    'clientOptions' => [
+    'source' => \app\models\Profile::getArrayForAutoComplete(false),
+    'autoFill' => true,
+    'minLength' => '0',
+    'select' => new JsExpression('function(event, ui) {
+    location.href = $("#urlTo").val() + "/?id=" + ui.item.id;
+    $("#label").html("Клиент");
+    $("#id").val(ui.item.id);
+    //               alert($(this).val());
+    $("#username").val(ui.item.phone);
+    $("#phone2").val(ui.item.phone2);
+    $("#email").val(ui.item.email);
+    $("#email2").val(ui.item.email2);
+    $("#name").val(ui.item.name);
+    $("#surname").val(ui.item.surname);
+    $("#patrinimic").val(ui.item.patrinimic);
+    var id = ui.item.id;
+    }'),
+    'response' => new JsExpression('function(event, ui) {
+    $("#username").val($(this).val());
+    }'),
+    'change' => new JsExpression('function(event, ui) {
+    if(!ui.item) {
+    $("#label").html("Новый клиент");
+    $("#id").val("");
+    //                       $("#username").val("");
+    $("#phone2").val("");
+    $("#email").val("");
+    $("#email2").val("");
+    $("#name").val("");
+    $("#surname").val("");
+    $("#patrinimic").val("");
+    $("#surname").focus();
 
+    }
+    }'),
+    ],
+    'options' => [
+    'id' => 'search-all',
+    'class' => 'form-control',
+    'placeholder' => Yii::t('app', 'Поиск...'),
+    ]
+    ]);
+    ?>
     <div class="container">
         <br><br>
         <?php if(Yii::$app->session->hasFlash('success')): ?>
