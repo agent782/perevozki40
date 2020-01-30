c2c30251bb54b0f6933d86d773fd8564a061a6ec
work <79206167111@ya.ru>
2020-01-30 14:50:55 +0300

    bbb

---
+++ modules/admin/views/test/index.php
@@ -0,0 +1,9 @@
+<?php
+/* @var $this yii\web\View */
+use yii\bootstrap\Html;
+?>
+<h3>Tests</h3>
+
+<p>
+    <?=Html::a('Подбор машин к заказу', '/admin/test/find-vehicles', ['class' => 'btn']);?>
+</p>
