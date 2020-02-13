c2e44ba8b8e3f4dbd22465fb0e3da3b1e387bf91
work <79206167111@ya.ru>
2020-02-13 08:28:43 +0300

    bbb

--- modules/admin/controllers/TestController.php
+++ modules/admin/controllers/TestController.php
@@ -42,7 +42,9 @@ class TestController extends \yii\web\Controller
             'id_order', 'number'
         );
         $model->id_order = $id_order;
-        $dataProvider = new ArrayDataProvider();
+        $dataProvider = new ArrayDataProvider([
+            'pagination' => ['pageSize' => 100]
+        ]);
 
 //        $dataProvider = new ArrayDataProvider();
         if($model->load(\Yii::$app->request->post())){
@@ -50,7 +52,6 @@ class TestController extends \yii\web\Controller
             if($order){
                 set_time_limit(200);
                 $dataProvider->allModels = $order->getSortSuitableVehicles();
-                $dataProvider->pagination->pageSize(100);
 //                return var_dump($dataProvider->allModels);
             } else {
                 functions::setFlashWarning('Заказ не найден!');
