cfeb9120b20d4a291220c6460ebb2ecd49171032
work <79206167111@ya.ru>
2020-02-20 09:47:27 +0300

    bbb

--- controllers/VehicleController.php
+++ controllers/VehicleController.php
@@ -608,7 +608,9 @@ class VehicleController extends Controller
             $user = Yii::$app->user->identity;
         }
 
-        $vehicles = $user->vehicles;
+        $vehicles = $user->getVehicles()
+            ->andWhere(['in', 'status', [Vehicle::STATUS_ACTIVE, Vehicle::STATUS_ONCHECKING]])
+            ->all();
         $Vehicles = [];
         $ids_vehicles = '';
         if($vehicles) {
