4abeb80c060614c021fc52009e22afd07c2238e0
work <79206167111@ya.ru>
2020-07-20 14:50:42 +0300

    bbb

--- views/order/auto-find.php
+++ views/order/auto-find.php
@@ -11,19 +11,19 @@ $this->registerJs($script);
 ?>
 <?php Pjax::begin([
     'id' => 'auto-find',
-    'timeout' => 1000
+//    'timeout' => 1000
 ]);?>
 
 <div class="container">
     <br><br>
     <div class="h2"> <?= $time;?></div>
 </div>
-<?//= Html::a("Обновить", ['/order/auto-find'],
-//    [
-//        'hidden' => true,
-//        'id' => 'refreshButton',
-//    ])
-//?>
+<?= Html::a("Обновить", ['/order/auto-find'],
+    [
+        'hidden' => true,
+        'id' => 'refreshButton',
+    ])
+?>
 
 <?php Pjax::end()?>
 
