88e4d42d32d37172d6ffd2bebbeef26179ce9f29
work <79206167111@ya.ru>
2020-07-21 10:50:38 +0300

    bbb

--- views/order/create5.php
+++ views/order/create5.php
@@ -171,6 +171,8 @@ Icon::map($this);
     </div>
 
 <div class="col-lg-12">
+    <?= $form->field($modelOrder, 'hide')->checkbox()->label('Скрыть
+    ')?>
     <?= Html::a('Назад', \yii\helpers\Url::previous(), ['class' => 'btn btn-warning'])?>
     <?=
     (!Yii::$app->user->isGuest)
