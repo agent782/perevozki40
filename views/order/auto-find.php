534c96a17ce2f6765128f4347dad80b3188e19d1
work <79206167111@ya.ru>
2020-07-24 11:11:18 +0300

    bbb

--- views/order/auto-find.php
+++ views/order/auto-find.php
@@ -1,6 +1,9 @@
 <?php
     use yii\bootstrap\Html;
     use yii\widgets\Pjax;
+    use app\models\Vehicle;
+    use app\components\functions\functions;
+    use app\models\CalendarVehicle;
 
 $script = <<< JS
 
@@ -29,6 +32,19 @@ $this->registerJs($script);
         'pjax' => false,
         'columns' => [
             'id',
+            [
+                'label' => 'Статус',
+                'format' => 'raw',
+                'value' => function (Vehicle $vehicle) use ($modelOrder){
+                    $date_day = functions::DayToStartUnixTime($modelOrder->datetime_start);
+                    $calendarVehicle = $vehicle->getCalendarVehicle()
+                        ->andWhere(['date' => $date_day])->one();
+                    if(!$calendarVehicle){
+                        $calendarVehicle = new CalendarVehicle();
+                    }
+                    return $calendarVehicle->status;
+                }
+            ]
         ]
     ])
 ?>
