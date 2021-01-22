66f7830517817479a09b4266362a8349bcddc424
work <79206167111@ya.ru>
2021-01-22 12:19:34 +0300

    bbb

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use Yii;
 use app\models\Profile;
 use app\models\User;
 use yii\helpers\Json;
@@ -15,6 +16,36 @@ class ApiController extends ActiveController
 
     public function actionViewer(){
         return (Profile::findOne(['id_user' => '1']));
+    }
+
+    public function actionLogin(){
+        $request = Yii::$app->request;
+        $username = $request->post('phone');
+        $password = $request->post('password');
+
+//        $username = '1111111111';
+//        $password = '123456';
+
+        $return = [
+            'userid' => null,
+            'name' => null
+        ];
+
+        Yii::$app->response->format = Response::FORMAT_JSON;
+
+        if($username){
+            $User = User::findOne(['username' => $username]);
+            if($User){
+                if($User->validatePassword($password)){
+                    $return['userid'] = $User->id;
+                    $return['name'] = $User->profile->name;
+                    return $return;
+                }
+            }
+        }
+
+        return $return;
+
     }
 
 
