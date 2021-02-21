b1d6196c337370185ab2c50c31ba23dd08f5f0d3
agent782 <79206167111@yandex.ru>
2021-02-21 22:05:27 +0300

    home1

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -101,14 +101,15 @@ public function beforeAction($action)
 
         if($request->firebase_id){
          //   if(is_array($User->firebase_ids) && in_array($request->firebase_id, $User->firebase_ids)){
-		if(($key=array_search($request->firebase_id, $User->firebase_ids)) !== false){ 
-			$firebase_ids = $User->firebase_ids;
-            unset($firebase_ids[$key]);
-			$User->firebase_ids = $firebase_ids;
-			if(!$User->firebase_ids) $User->firebase_ids = null;
-			$User->scenario = $User::SCENARIO_SAVE;
-               		 $User->save();
-		}
+            if(($key=array_search($request->firebase_id, $User->firebase_ids)) !== false){
+                return 1;
+                $firebase_ids = $User->firebase_ids;
+                unset($firebase_ids[$key]);
+                $User->firebase_ids = $firebase_ids;
+                if(!$User->firebase_ids) $User->firebase_ids = null;
+                $User->scenario = $User::SCENARIO_SAVE;
+                $User->save();
+            }
 
            // }
         }
