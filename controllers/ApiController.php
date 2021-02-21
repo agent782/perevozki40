4ccac80abbfa701799ebff3b0e1f2240b7655c64
agent782 <79206167111@yandex.ru>
2021-02-21 21:42:45 +0300

    home1

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -56,7 +56,7 @@ public function beforeAction($action)
                 if($User->validatePassword($password)){
                     if($request->firebase_id){
                         if(is_array($User->firebase_ids) && !in_array($request->firebase_id, $User->firebase_ids)){
-				firebase_ids =  $User->firebase_ids[];
+				$firebase_ids =  $User->firebase_ids;
                             	$firebase_ids[] = $request->firebase_id;
 				 $User->firebase_ids[] =  firebase_ids;
                         } else {
