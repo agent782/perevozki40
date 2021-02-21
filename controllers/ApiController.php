ab00ca8ffdc00315e8904e718f19841ad8486505
agent782 <79206167111@yandex.ru>
2021-02-21 22:13:19 +0300

    home1

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -99,7 +99,8 @@ public function beforeAction($action)
         $User = Yii::$app->user->identity;
 //	return $User->firebase_ids;
 //        return $request->firebase_id;
-    return $key=array_search($request->firebase_id, $User->firebase_ids);
+        $key=array_search($request->firebase_id, $User->firebase_ids);
+        return $key !==false;
         if($request->firebase_id){
          //   if(is_array($User->firebase_ids) && in_array($request->firebase_id, $User->firebase_ids)){
             if(($key=array_search($request->firebase_id, $User->firebase_ids)) !== false){
