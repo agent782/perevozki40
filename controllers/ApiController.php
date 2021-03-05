5d9cace2a8b1db7ad5162e90de89fcc280c66985
work <79206167111@ya.ru>
2021-03-05 14:04:45 +0300

    bbb

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -319,7 +319,7 @@ public function beforeAction($action)
                         $return ['new'][] = [
                             'id' => $order->id,
                             'date' => $order->datetime_start,
-                            'day_week' => functions::rus_date('l', 13243534654),
+                            'day_week' => functions::rus_date('l', strtotime($order->datetime_start)),
                             'date_valid' => $order->valid_datetime,
                             'route' => $route->fullRoute,
                             'info' => $order->getShortInfoForClient(false),
